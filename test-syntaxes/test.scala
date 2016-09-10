b4acc89a1c1677828b7823f567729e7d84f63761
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2016-09-10 14:56:22 -0500

    make punk gutter conditional

---
+++ test-syntaxes/test.scala
@@ -0,0 +1,86 @@
+import swing._
+import event._
+
+
+// val lara = Person("Lara", false)
+
+
+val bob = Person("Bob", true)
+val julie = Person("Julie", false, lara, bob)
+val persons = List(lara, bob, julie)
+
+
+class Rational(n: Int, d: Int) {
+
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  def + (that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def + (i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    new Rational(numer - i * denom, denom)
+
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def * (i: Int): Rational =
+    new Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
+
+  override def toString = numer +"/"+ denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+}
+
+
+
+
+object TempConverter extends SimpleGUIApplication {
+  def top = new MainFrame {
+    title = "Celsius/Fahrenheit Converter"
+    object celsius extends TextField { columns = 5 }
+    object fahrenheit extends TextField { columns = 5 }
+    contents = new FlowPanel {
+      contents += celsius
+      contents += new Label(" Celsius  =  ")
+      contents += fahrenheit
+      contents += new Label(" Fahrenheit")
+      border = Swing.EmptyBorder(15, 10, 10, 10)
+    }
+    listenTo(celsius, fahrenheit)
+    reactions += {
+      case EditDone(`fahrenheit`) =>
+        val f = fahrenheit.text.toInt
+        val c = (f - 32) * 5 / 9
+        celsius.text = c.toString
+      case EditDone(`celsius`) =>
+        val c = celsius.text.toInt
+        val f = c * 9 / 5 + 32
+        fahrenheit.text = f.toString
+    }
+  }
+}
